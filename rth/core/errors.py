ed4093c4bbb4d4ffc9fde22e5c03939b9608f445
BioTheWolff <contact.biowolf@gmx.fr>
2020-07-31 00:08:33 +0200

    rewrite errors.py doc

--- rth/core/errors.py
+++ rth/core/errors.py
@@ -3,53 +3,68 @@
 #  The package which contains all the exceptions of the program
 
 
-## The subnetworks dictionary is wrongly formed
-#
-#  Is thrown when the subnetworks dictionary is wrongly formatted.
 class WronglyFormedSubnetworksData(Exception):
+    """
+    The subnetworks dictionary is wrongly formed
+
+    Is thrown when the subnetworks dictionary is wrongly formatted.
+    """
 
     def __str__(self):
         return "The subnetworks data given is wrongly formed or missing information. Please verify said data."
 
 
-## The routers dictionary is wrongly formed
-#
-#  Is thrown when the routers dictionary is wrongly formatted.
 class WronglyFormedRoutersData(Exception):
+    """
+    The routers dictionary is wrongly formed
+
+    Is thrown when the routers dictionary is wrongly formatted.
+    """
 
     def __str__(self):
         return "The routers data given is wrongly formed or missing information. Please verify said data."
 
 
-## The links dictionary is wrongly formed
-#
-#  Is thrown when the links dictionary is wrongly formatted.
 class WronglyFormedLinksData(Exception):
+    """
+    The links dictionary is wrongly formed
+
+    Is thrown when the links dictionary is wrongly formatted.
+    """
 
     def __str__(self):
         return "The link data given is wrongly formed or missing information. Please verify said data."
 
 
-## The program misses some data
-#
-#  Thrown when a file misses some crucial data (subnetworks, routers or links).
-#  Currently not used, but implemented for later.
 class MissingDataParameter(Exception):
+    """
+    The program misses some data
+
+    Thrown when a file misses some crucial data (subnetworks, routers or links).
+    Currently not used, but implemented for later.
+    """
 
     def __str__(self):
         return "Missing one of the required data (subnetworks, routers or links) and could not find an " \
                "already-existing network instance"
 
 
-## A subnetwork overlaps another
-#
-#  Thrown when a subnetwork tries to overlap an already-existing subnetwork in the virtual network instance.
 class OverlappingError(Exception):
+    """
+    A subnetwork overlaps another
 
-    ##
-    #  @param new The new subnetwork range
-    #  @param existing The existing subnetwork range
+    Thrown when a subnetwork tries to overlap an already-existing subnetwork in the virtual network instance.
+    """
+
     def __init__(self, new, existing):
+        """
+        Init the new Exception
+
+        Args:
+            new: The new subnetwork range
+            existing: The existing subnetwork range
+        """
+
         self.new_range = f"{new['start']} - {new['end']}"
         self.existing_range = f"{existing['start']} - {existing['end']}"
 
@@ -57,28 +72,37 @@ class OverlappingError(Exception):
         return f"Range {self.new_range} is overlapping range {self.existing_range}"
 
 
-## No delay is allowed on the router
-#
-#  Thrown when the program reads a delay set on a router, but equitemporality is set to True.
-#  Currently not used because non equitemporality is not implemented yet.
 class NoDelayAllowed(Exception):
+    """
+    No delay is allowed on the router
+
+    Thrown when the program reads a delay set on a router, but equitemporality is set to True.
+    Currently not used because non equitemporality is not implemented yet.
+    """
 
     def __str__(self):
         return "No delay allowed when equitemporality is set to True. " \
                "Pass equitemporality=False when instancing the Dispatcher class"
 
 
-## The provided IP is already attributed
-#
-#  Thrown when the provided IP is already attributed to another router on the subnetwork
 class IPAlreadyAttributed(Exception):
+    """
+    The provided IP is already attributed
 
-    ##
-    #  @param subnet_name The name of the subnetwork.
-    #  @param ip The concerned IP.
-    #  @param attributed The name of the router that possesses this IP.
-    #  @param tried_to_attribute The name of the router which tried to get this IP.
+    Thrown when the provided IP is already attributed to another router on the subnetwork.
+    """
+
     def __init__(self, subnet_name, ip, attributed, tried_to_attribute):
+        """
+        Init the new Exception
+
+        Args:
+            subnet_name: The name of the subnetwork.
+            ip: The concerned IP.
+            attributed: The name of the router that possesses this IP.
+            tried_to_attribute: The name of the router which tried to get this IP.
+        """
+
         self.name = subnet_name
         self.ip = ip
         self.attributed = attributed
@@ -89,30 +113,44 @@ class IPAlreadyAttributed(Exception):
                f"'{self.attributed}'; Tried to give it to router '{self.tried}'"
 
 
-## The name of the subnetwork or router already exists
-#
-#  Thrown when the user tries to give two subnetworks or routers the same name.
 class NameAlreadyExists(NameError):
+    """
+    The name of the subnetwork or router already exists
 
-    ##
-    #  @param name The name that already exists.
+    Thrown when the user tries to give two subnetworks or routers the same name.
+    """
+
     def __init__(self, name):
+        """
+        Init the new Exception
+
+        Args:
+            name: The name that already exists.
+        """
+
         self.name = name
 
     def __str__(self):
         return f"Name '{self.name}' already exists"
 
 
-## A subnetwork is unreachable
-#
-#  Thrown during the AntsDiscovery process if a subnetwork is unreachable from the master router.
 class UnreachableNetwork(Exception):
+    """
+    A subnetwork is unreachable
 
-    ##
-    #  @param name The name of the unreachable subnetwork.
-    #  @param cidr Its CIDR.
-    #  @param total The total number of unreachable subnetworks.
+    Thrown during the AntsDiscovery process if a subnetwork is unreachable from the master router.
+    """
+
     def __init__(self, name, cidr, total):
+        """
+        Init the new Exception
+
+        Args:
+            name: The name of the unreachable subnetwork.
+            cidr: Its CIDR.
+            total: The total number of unreachable subnetworks.
+        """
+
         self.name = name
         self.cidr = cidr
         self.total = total
@@ -122,15 +160,22 @@ class UnreachableNetwork(Exception):
                f"Total number of unreachable subnetworks: {self.total}"
 
 
-## There was an error with the master router
-#
-#  Thrown when there is a problem with the master router.
-#  Currently, is thrown when the network lacks a master router, or has more than one.
 class MasterRouterError(Exception):
+    """
+    There was an error with the master router
 
-    ##
-    #  @param no_internet Whether it concerns the network having no internet or more than one master router.
+    Thrown when there is a problem with the master router.
+    Currently, is thrown when the network lacks a master router, or has more than one.
+    """
+
     def __init__(self, no_internet=False):
+        """
+        Init the new Exception
+
+        Args:
+            no_internet: Whether it concerns the network having no internet or more than one master router.
+        """
+
         if no_internet:
             self.text = "There is no connection to the internet on this network. Please connect one router."
         else:
